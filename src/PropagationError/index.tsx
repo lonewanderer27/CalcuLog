336cb67118f84c42d5fcc9da1be92f6621d0bf35
lonewanderer27 <adrianejames@gmail.com>
2023-03-04 17:10:15 +0800

    initial layout

---
+++ src/PropagationError/index.tsx
@@ -0,0 +1,7 @@
+export default function PropagationError() {
+    return (
+        <div className="propagationError">
+            <h3>Propagation Error</h3>
+        </div>
+    )
+}
---
+++ src/TaylorMaclaurin/index.tsx
@@ -0,0 +1,7 @@
+export default function TaylorMaclaurin() {
+    return (
+        <div className="taylorMaclaurin">
+            <h3>Taylor Maclaurin</h3>
+        </div>
+    )
+}
